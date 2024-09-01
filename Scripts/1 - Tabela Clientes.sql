4073d344fa2970b68729229c43d03b31098c90e4
Jefferson Nóbrega <jeffersonnobrega@gmail.com>
2024-08-31 23:01:40 -0300

    atualizando o diretório

--- Scripts/1 - Tabela Clientes.sql
+++ Scripts/1 - Tabela Clientes.sql
@@ -15,7 +15,7 @@ INSERT INTO Clientes VALUES ('Terri','Duffy','email@email.com',1,'Jan 24 2008 12
 INSERT INTO Clientes VALUES ('Roberto','Tamburello','email@email.com',0,'Nov  4 2007 12:00AM')
 INSERT INTO Clientes VALUES ('Rob','Walters','email@email.com',0,'Nov 28 2007 12:00AM')
 INSERT INTO Clientes VALUES ('Gail','Erickson','email@email.com',0,'Dec 30 2007 12:00AM')
-INSERT INTO Clientes VALUES ('Jossef','Goldberg','email@email.com',0,'Dec 16 2013 12:00AM')
+INSERT INTO Clientes VALUES ('Jossef','Goldberg','email@email.com',0,'Dec 30 2007 12:00AM')
 INSERT INTO Clientes VALUES ('Dylan','Miller','email@email.com',2,'Feb  1 2009 12:00AM')
 INSERT INTO Clientes VALUES ('Diane','Margheim','email@email.com',0,'Dec 22 2008 12:00AM')
 INSERT INTO Clientes VALUES ('Gigi','Matthew','email@email.com',0,'Jan  9 2009 12:00AM')
